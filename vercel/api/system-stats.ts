4a947d42b1d696ec004fd4602fcc7906867f70b8
Gino Messmer <git@ginomessmer.me>
2020-07-12 19:57:26 +0200

    Remove junk

--- vercel/api/system-stats.ts
+++ vercel/api/system-stats.ts
@@ -2,8 +2,5 @@ import { NowRequest, NowResponse } from "@vercel/node";
 import fs from 'fs';
 
 export default (req: NowRequest, res: NowResponse) => {
-  const rawData = fs.readFileSync('./system-stats.json').toString();
 
-  const stats = JSON.parse(rawData);
-  res.json(stats);
 }
