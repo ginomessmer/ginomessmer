e0bcbd4a3326b03f339e30bd9de90d79f4c6a98f
Gino Messmer <git@ginomessmer.me>
2020-07-12 20:59:02 +0200

    Add render method

--- vercel/api/system-stats.ts
+++ vercel/api/system-stats.ts
@@ -1,7 +1,18 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+import { renderToString } from 'react-dom/server';
+
 import { getSystemStats } from "../services/system-stats";
+import CpuCard from "../components/CpuCard";
 
 export default async (req: NowRequest, res: NowResponse) => {
-  const stats = await getSystemStats();
-  res.json(stats.data);
+  const response = await getSystemStats();
+  
+  res.setHeader("Content-Type", "image/svg+xml");
+  res.setHeader("Cache-Control", "no-cache");
+
+  const card = renderToString(CpuCard({
+
+  }));
+
+  return res.status(200).send(card);
 }
---
+++ vercel/components/CpuCard.tsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+import Svg from "./Svg";
+
+const CpuCard = (props: {}) => <Svg width="256" height="64">
+  <p>Test</p>
+</Svg>
+
+export default CpuCard;
