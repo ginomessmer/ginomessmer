ddb0679392a1515cdf25b33b451d968e66272282
Gino Messmer <git@ginomessmer.me>
2020-07-12 20:17:26 +0200

    Upload system stats to gist

--- daemon/index.js
+++ daemon/index.js
@@ -1,10 +1,39 @@
 const os = require('os');
+const gh = require('@octokit/rest');
 
-console.log({
-  cpus: os.cpus(),
-  ram: os.uptime(),
-  mem: {
-    total: os.totalmem(),
-    free: os.freemem()
-  }
+const secrets = require('./secrets.json');
+let system = {};
+
+const octokit = new gh.Octokit({
+  auth: secrets.pat
 });
+
+console.log('Daemon started');
+
+setInterval(() => {
+  uploadSystemStats().then(x => {
+    console.log('Gist updated');
+  });
+
+}, 5 * 1000);
+
+const uploadSystemStats = () => {
+  system = {
+    version: new Date(),
+    cpus: os.cpus(),
+    uptime: os.uptime(),
+    mem: {
+      total: os.totalmem(),
+      free: os.freemem()
+    }
+  };
+
+  return octokit.gists.update({
+    gist_id: secrets.gistId,
+    files: {
+      'system-stats.json': {
+        content: JSON.stringify(system, null, 2)
+      }
+    }
+  });
+}
