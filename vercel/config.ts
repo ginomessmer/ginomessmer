f7bad5bf850f9464cea7358d13ca69e7dbe9aa37
Gino Messmer <git@ginomessmer.me>
2020-07-12 20:50:34 +0200

    Remove commit ID from gist

--- vercel/config.ts
+++ vercel/config.ts
@@ -1,3 +1,3 @@
 export default {
-  SYSTEM_STATS_ENDPOINT: "https://gist.githubusercontent.com/ginomessmer/65362215a931bfd470dc75694ed0da0e/raw/90930ebe957cfec33c2a118c254299e47d87a966/system-stats.json"
+  SYSTEM_STATS_ENDPOINT: "https://gist.githubusercontent.com/ginomessmer/65362215a931bfd470dc75694ed0da0e/raw/system-stats.json"
 }
