24884326e5c9485ab6f3d32d153b3fa9631cd5a2
Gino Messmer <git@ginomessmer.me>
2020-07-12 20:48:35 +0200

    Fix config

--- vercel/services/system-stats.ts
+++ vercel/services/system-stats.ts
@@ -2,5 +2,5 @@ import Axios from "axios";
 import * as config from '../config';
 
 export const getSystemStats = () => {
-  return Axios.get(config.SYSTEM_STATS_ENDPOINT);
+  return Axios.get(config.default.SYSTEM_STATS_ENDPOINT);
 }
