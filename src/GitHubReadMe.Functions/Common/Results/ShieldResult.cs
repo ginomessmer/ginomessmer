c33c924c118fea10bdf1d5f7e5e782096ba1d2f2
Gino Messmer <git@ginomessmer.me>
2020-07-17 20:06:31 +0200

    Add ShieldResult

---
+++ src/GitHubReadMe.Functions/Common/Results/ShieldResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace GitHubReadMe.Functions.Common.Results
+{
+    public class ShieldResult : IActionResult
+    {
+        public string Title { get; set; }
+        public string Value { get; set; }
+        public string Color { get; set; }
+
+
+        public ShieldResult(string title, string value, string color = "green")
+        {
+            Title = title;
+            Value = value;
+            Color = color;
+        }
+
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            using var httpClient = new HttpClient();
+            context.HttpContext.Response.Headers.Add("Cache-Control", "s-maxage=1, stale-while-revalidate");
+                
+            var stream = await httpClient.GetStreamAsync($"https://img.shields.io/badge/{Title}-{Value}-{Color}");
+            var result = new FileStreamResult(stream, "image/svg+xml");
+
+            await result.ExecuteResultAsync(context);
+        }
+    }
+}
--- src/GitHubReadMe.Functions/Shields/HitsShield.cs
+++ src/GitHubReadMe.Functions/Shields/HitsShield.cs
@@ -1,8 +1,7 @@
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using GitHubReadMe.Functions.Common.Entities;
+using GitHubReadMe.Functions.Common.Results;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos.Table;
@@ -11,20 +10,20 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace GitHubReadMe.Functions.Badges
+namespace GitHubReadMe.Functions.Shields
 {
-    public class HitsBadge
+    public class HitsShield
     {
         private readonly IConfiguration _configuration;
 
-        public HitsBadge(IConfiguration configuration)
+        public HitsShield(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
-        [FunctionName("HitsBadge")]
+        [FunctionName("HitsShield")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "shields/hits")] HttpRequest req,
             [Table("GhRmHits")] CloudTable table,
             ILogger log)
         {
@@ -38,14 +37,7 @@ namespace GitHubReadMe.Functions.Badges
             // Retrieve current count
             var count = table.ExecuteQuery(new TableQuery()).Count();
 
-            using (var httpClient = new HttpClient())
-            {
-                // Proxy response
-                var stream = await httpClient.GetStreamAsync($"https://img.shields.io/badge/views-{count}-green");
-
-                req.HttpContext.Response.Headers.Add("Cache-Control", "s-maxage=1, stale-while-revalidate");
-                return new FileStreamResult(stream, "image/svg+xml");
-            }
+            return new ShieldResult("views", count.ToString(), "green");
         }
     }
 }
